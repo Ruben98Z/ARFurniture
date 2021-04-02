f40ef427abf8ca4851e8c919300997779207a470
Ruben98Z <rubenzj98@gmail.com>
2021-04-02 11:53:49 +0200

    Dynamically add models

---
+++ Assets/Scripts/FurnitureManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class FurnitureManager : MonoBehaviour
+{
+    #region PUBLIC_MEMBERS
+
+    public GameObject furniture;
+    public TouchHandler touchHandler;
+
+    #endregion // PUBLIC MEMBERS
+
+    [SerializeField] PlaneFinderBehaviour planeFinder = null;
+    ContentPositioningBehaviour contentPositioningBehaviour;
+    AnchorBehaviour planeAnchor;
+
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+
+    public void ChangeAnchor(string nameAnchor)
+    {
+        this.contentPositioningBehaviour = this.planeFinder.GetComponent<ContentPositioningBehaviour>();
+        this.planeAnchor = this.furniture.transform.Find(nameAnchor).GetComponent<AnchorBehaviour>();
+        this.contentPositioningBehaviour.AnchorStage = this.planeAnchor;
+    }
+
+    public void ChangeTouchFurniture(GameObject newFurniture)
+    {
+        this.touchHandler.TouchFurniture(newFurniture.transform);
+    }
+
+}
