447a91e23bdc4332037d1d70448aee5906c6bbae
Ruben98Z <rubenzj98@gmail.com>
2021-04-02 11:52:55 +0200

    Move models
    Models can be moved in the XZ plane

---
+++ Assets/Scripts/ProductPlacement.cs
@@ -0,0 +1,97 @@
+﻿using UnityEngine;
+using Vuforia;
+
+public class ProductPlacement : MonoBehaviour
+{
+    #region PUBLIC_MEMBERS
+    public bool IsPlaced { get; private set; }
+    public bool AnchorIsTracking { get; private set; }
+    #endregion // PUBLIC_MEMBERS
+
+
+    #region PRIVATE_MEMBERS
+    [Header("Augmentation Objects")]
+    [SerializeField] GameObject chair = null;
+
+    //[Header("Control Indicators")]
+    //[SerializeField] GameObject translationIndicator = null;
+    //[SerializeField] GameObject rotationIndicator = null;
+
+    [Header("Augmentation Size")]
+    [Range(0.1f, 2.0f)]
+    [SerializeField] float productSize = 0.65f;
+
+    MeshRenderer chairRenderer;
+
+    Camera mainCamera;
+    Ray cameraToPlaneRay;
+    RaycastHit cameraToPlaneHit;
+
+    float augmentationScale;
+    Vector3 productScale;
+    string floorName;
+
+    #endregion // PRIVATE_MEMBERS
+
+
+    #region MONOBEHAVIOUR_METHODS
+    void Start()
+    {
+        this.mainCamera = Camera.main;
+
+        SetupFloor();
+
+
+        this.augmentationScale = VuforiaRuntimeUtilities.IsPlayMode() ? 0.1f : this.productSize;
+
+        this.productScale =
+            new Vector3(this.augmentationScale,
+                        this.augmentationScale,
+                        this.augmentationScale);
+
+        this.chair.transform.localScale = this.productScale;
+    }
+
+
+    void Update()
+    {
+
+            if (TouchHandler.IsSingleFingerDragging || (VuforiaRuntimeUtilities.IsPlayMode() && Input.GetMouseButton(0)))
+            {
+                    this.cameraToPlaneRay = this.mainCamera.ScreenPointToRay(Input.mousePosition);
+
+                    if (Physics.Raycast(this.cameraToPlaneRay, out this.cameraToPlaneHit))
+                    {
+                        if (this.cameraToPlaneHit.collider.gameObject.name == floorName)
+                        {
+                            this.chair.PositionAt(this.cameraToPlaneHit.point);
+                        }
+                    }
+                
+            }
+    }
+    #endregion // MONOBEHAVIOUR_METHODS
+
+
+
+    #region PRIVATE_METHODS
+
+    void SetupFloor()
+    {
+        if (VuforiaRuntimeUtilities.IsPlayMode())
+        {
+            this.floorName = "Emulator Ground Plane";
+        }
+        else
+        {
+            this.floorName = "Floor";
+            GameObject floor = new GameObject(this.floorName, typeof(BoxCollider));
+            floor.transform.SetParent(this.chair.transform.parent);
+            floor.transform.SetPositionAndRotation(Vector3.zero, Quaternion.identity);
+            floor.transform.localScale = Vector3.one;
+            floor.GetComponent<BoxCollider>().size = new Vector3(100f, 0, 100f);
+        }
+    }
+    #endregion // PRIVATE_METHODS
+
+}
